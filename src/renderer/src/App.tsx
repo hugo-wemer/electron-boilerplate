dc2be60a07a5218ba86391d2303ff1ca1b99d3bd
Hugo Silva <hugo.silva@treetech.local>
2025-01-30 13:29:59 -0300

    chore: use tailwindcss

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,4 @@
+import './styles/global.css'
 export function App() {
-  return <h1 style={{ fontFamily: 'Inter' }}>Hello world</h1>
+  return <h1 className='text-violet-400 font-bold'>Hello world</h1>
 }
